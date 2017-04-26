e704f14c83c1722b353a5849eb26d1176d1edd9b
Betty Mutai <bettymutai00@gmail.com>
2017-04-26 10:55:12 +0300

    commiting

--- scripts.js
+++ scripts.js
@@ -1,8 +1,28 @@
-var add = function(number1, number2) {
-        return number1 + number2;
-      };
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
 
-      var number1 = parseInt(prompt("Enter a number:"));
-      var number2 = parseInt(prompt("Enter another number:"));
-      var result = add(number1, number2);
-      alert(result);
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var result = add(number1, number2);
+alert(result);
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+alert(subtract(number1, number2));
+
+var mul = function(number1, number2) {
+  return number1 * number2;
+};
+alert(mul(number1, number2));
+
+var div = function(number1, number2) {
+  return number1 / number2;
+};
+alert(div(number1, number2));
+
+var modulus = function(number1, number2) {
+  return number1 % number2;
+};
+alert(modulus(number1, number2));
